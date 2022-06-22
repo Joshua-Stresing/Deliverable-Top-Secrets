34c6fa3c354c4a153906a630070bb0d9b4e0ca38
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-22 16:52:54 -0700

    create slice should e working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,31 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+
+describe('secret routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('creates a new user', async () => {
+    //api/v1 is standard naming for versions
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
