59c169d7c68da3e265abbf02ff1423d61d904693
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-22 17:22:56 -0700

    current user slice taken care of

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,29 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Test',
   lastName: 'User',
   email: 'test@example.com',
   password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('secret routes', () => {
@@ -25,6 +42,17 @@ describe('secret routes', () => {
       firstName,
       lastName,
       email,
+    });
+  });
+
+  it('returns the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
   });
 
